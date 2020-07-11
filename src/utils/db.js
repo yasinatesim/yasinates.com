54cdaf7c0b65fb830e7bab0f6be0080249d0782b
Yasin ATEŞ <yasinatesim@gmail.com>
2020-07-11 17:47:33 +0300

    fix(api): update file path

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,12 +1,13 @@
 import low from 'lowdb';
 import FileSync from 'lowdb/adapters/FileSync';
+import path from 'path';
 
 // My Posts API Cache
-const posts = new FileSync('../../pages/api/cache/posts.json');
+const posts = new FileSync(path.resolve('../../pages/api/cache/posts.json'));
 const postsDB = low(posts);
 
 // My Repos API Cache
-const repos = new FileSync('../../pages/api/cache/repos.json');
+const repos = new FileSync(path.resolve('../../pages/api/cache/repos.json'));
 const reposDB = low(repos);
 
 postsDB.defaults({ posts: [] }).write();
