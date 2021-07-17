731808c0ed2c89f4095643db614a910ed86f7de5
Yasin ATEŞ <yasinatesim@gmail.com>
2021-07-17 22:31:07 +0300

    chore: add iframe #1

--- pages/[post].js
+++ pages/[post].js
@@ -1,5 +1,8 @@
 import PropTypes from 'prop-types';
 
+// Utils
+import axios from 'axios';
+
 // Components
 import Post from '@/universal/containers/post';
 
@@ -9,13 +12,22 @@ import { posts } from '../public/api/cache/posts.json';
  * This is post detail page
  * @param {Object} post - This is single post data
  */
-function PostPage({ post }) {
+function PostPage({ post, githubGists }) {
   const content = post ? post.content : null;
+
+
+  const deneme = Object.values(githubGists);
+  console.log('deneme:', deneme)
 
   return (
     <div className="container">
       {post ? (
-        <Post sourceWebsite={post.source_website} thumbnail={post.thumbnail} title={post.title} content={content} />
+        <>
+          <>
+            {/* <script src="https://gist.github.com/yasinatesim/489040a2973d6e15bb2ddee2933dc970.js?file=index.pug" /> */}
+          </>
+          <Post sourceWebsite={post.source_website} thumbnail={post.thumbnail} title={post.title} content={content} />
+        </>
       ) : (
         <div className="spinner">
           <div className="double-bounce1" />
@@ -36,12 +48,36 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
   const post = await posts.filter((p) => p.id === params.post);
+  const firstPost = post[0];
 
-  return { props: { post: post[0] } };
+  const gistUrls = firstPost.content.match(/https:\/\/medium\.com\/media\/(.*?)\/href/g);
+  const sameGistUrls = gistUrls.filter((item, index) => gistUrls.indexOf(item) === index);
+
+  const githubGists = await Promise.all(
+    sameGistUrls.map(async (item) => {
+      const result = await axios.get(item, {
+        headers: {
+          'Content-Type': 'text/html; charset=utf-8',
+          'Access-Control-Allow-Origin': '*',
+        },
+      });
+
+      const gistUrl = result.data
+        .match(/https:\/\/gist\.github.com\/yasinatesim\/(.*?)\?file=([a-zA-Z0-9.]+)/g)[0]
+        .split('?');
+
+      return {
+        [item]: `${gistUrl[0]}.js?${gistUrl[1]}`,
+      };
+    }),
+  );
+
+  return { props: { post: firstPost, githubGists } };
 }
 
 PostPage.propTypes = {
   post: PropTypes.object,
+  githubGists: PropTypes.array,
 };
 
 export default PostPage;
